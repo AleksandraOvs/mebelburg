4454ecffed3d6926738b792f38afc382966740e9
Aleksandraovs <aleksandra.ovsia@gmail.com>
2024-05-30 11:04:24 +0300

    eds

--- wp-content/themes/mebelburg/templates/sections/section-adv.php
+++ wp-content/themes/mebelburg/templates/sections/section-adv.php
@@ -5,15 +5,20 @@
             <li class="section-adv__grid__item border" style="background-color:#fff; background-image:url(<?php echo get_stylesheet_directory_uri() . '/images/adv1.svg' ?>); background-repeat:no-repeat; background-position: center;">
                
                 <div class="section-adv__grid__item__content">
-                    <div class="section-adv__grid__item__content__top"><p>03</p></div>
-                    <div class="section-adv__grid__item__content__bottom"></div>
+                    <div class="section-adv__grid__item__content__top color-black"><p>03</p></div>
+                    <div class="section-adv__grid__item__content__bottom color-black">этажа товаров
+для вашего дома</div>
                 </div>
             </li>
             <li class="section-adv__grid__item box2">
                 <img src="<?php echo get_stylesheet_directory_uri() . '/images/adv2.jpg' ?>" alt="" class="section-adv__grid__item__img">
                 <div class="section-adv__grid__item__content">
-                    <div class="section-adv__grid__item__content__top"></div>
-                    <div class="section-adv__grid__item__content__bottom"></div>
+                    <div class="section-adv__grid__item__content__top color-white">
+                        <p class="color-white">30 236 м<sup>2</sup></p>
+                    </div>
+                    <div class="section-adv__grid__item__content__bottom color-white">
+                        <p>обширного выбора<br>и вдохновения</p>
+                    </div>
                 </div>
             </li>
             <li class="section-adv__grid__item box3" style="background-color:transparent; background-image:url(<?php echo get_stylesheet_directory_uri() . '/images/adv3.svg' ?>); background-repeat:no-repeat; background-position: center;">
@@ -25,29 +30,35 @@
             </li>
             <li class="section-adv__grid__item bg-beige" style="background-image:url(<?php echo get_stylesheet_directory_uri() . '/images/adv4.svg' ?>); background-repeat:no-repeat; background-position: center;">
                 <div class="section-adv__grid__item__content">
-                    <div class="section-adv__grid__item__content__top"></div>
-                    <div class="section-adv__grid__item__content__bottom"></div>
+                    <div class="section-adv__grid__item__content__top">
+                        <p class="color-white">150+</p>
+                    </div>
+                    <div class="section-adv__grid__item__content__bottom">
+                        <p class="color-white">магазинов<br>и мебельных брендов</p>
+                    </div>
                 </div>
             </li>
             <li class="section-adv__grid__item" style="background-color: #f7f7f7;">
                 <img src="<?php echo get_stylesheet_directory_uri() . '/images/adv5.png' ?>" alt="" class="section-adv__grid__item__img">
                 <div class="section-adv__grid__item__content">
-                    <div class="section-adv__grid__item__content__top"></div>
-                    <div class="section-adv__grid__item__content__bottom"></div>
+                    <!-- <div class="section-adv__grid__item__content__top"></div> -->
+                    <div class="section-adv__grid__item__content__bottom">
+                        <p>Лекторий и мероприятия<br>о&nbsp;дизайне интерьера и&nbsp;ремонте</p></div>
                 </div>
             </li>
             <li class="section-adv__grid__item box6">
                 <img src="<?php echo get_stylesheet_directory_uri() . '/images/adv6.png' ?>" alt="" class="section-adv__grid__item__img">
                 <div class="section-adv__grid__item__content">
-                    <div class="section-adv__grid__item__content__top"></div>
-                    <div class="section-adv__grid__item__content__bottom"></div>
+                    <!-- <div class="section-adv__grid__item__content__top"></div> -->
+                    <div class="section-adv__grid__item__content__bottom">
+                        <p class="color-white">Мебель для любых комнат — от диванов<br>до кухонь и гардеробных</p></div>
                 </div>
             </li>
             <li class="section-adv__grid__item bg-purple" style="background-image:url(<?php echo get_stylesheet_directory_uri() . '/images/adv4.svg' ?>); background-repeat:no-repeat; background-position: center;">
                 
                 <div class="section-adv__grid__item__content">
-                    <div class="section-adv__grid__item__content__top"></div>
-                    <div class="section-adv__grid__item__content__bottom"></div>
+                    <!-- <div class="section-adv__grid__item__content__top"></div> -->
+                    <div class="section-adv__grid__item__content__bottom color-white">Всё для интерьера — от обоев<br>и полов до освещения и бытовой техники</div>
                 </div>
             </li>
         </ul>
