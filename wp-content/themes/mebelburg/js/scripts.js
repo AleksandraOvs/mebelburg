feeb134260b75c6b2f6f5b6c613a4409f51bfab2
Aleksandraovs <aleksandra.ovsia@gmail.com>
2024-05-30 09:10:07 +0300

    eds

--- wp-content/themes/mebelburg/js/scripts.js
+++ wp-content/themes/mebelburg/js/scripts.js
@@ -1,28 +1,39 @@
 document.addEventListener("DOMContentLoaded", () => {
-    
+
     let body = $('body');
     let menu = $('.container nav');
+    let textDefault = 'Меню';
+    let textOther = 'Закрыть';
 
-    if( window.innerWidth >= 1200 ){
-   
+    $('.header__inner__burger span').text(textDefault);
+
+    $(document).on('click', '.header__inner__burger', function () {
+
+        textDefault = textOther;
+        textOther = $('.header__inner__burger span').text();
+        $('.header__inner__burger span').text(textDefault);
+
+
+        $(this).toggleClass('_open');
+        menu.toggleClass('_open');
+        body.toggleClass('_fixed');
+
+        // document.querySelector('.header__inner__burger span').innerHTML = 'Закрыть';
+    });
+
+
+    if( window.innerWidth >= 1240 ){
     let navItemLength = document.querySelectorAll('nav .main-menu li').length;
     let ourLogo = document.querySelector('.container .header__inner__logo');
 
-    let menuItems = document.querySelectorAll('nav .main-menu li:nth-child(' + Math.floor(navItemLength / 2+1) + ')');
+    let menuItems = document.querySelectorAll('nav .main-menu li:nth-child(' + Math.floor(navItemLength / 2 + 1) + ')');
 
     for (var i = 0; i < menuItems.length; i++) {
         menuItems[i].after(ourLogo);
     }
-    }
+}
 
-    $(document).on('click', '.header__inner__burger', function () {
-        $(this).toggleClass('_open');
-        menu.toggleClass('_open');
-        body.toggleClass('_fixed');
-      });
-  
-
-    
-});
+}
+);
 
 
