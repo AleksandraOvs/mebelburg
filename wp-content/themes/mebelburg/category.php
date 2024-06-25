a77118e3a5d6521c10ee7b7ce5e1b236e79b9421
Aleksandraovs <aleksandra.ovsia@gmail.com>
2024-06-25 15:37:07 +0300

    eds

---
+++ wp-content/themes/mebelburg/category.php
@@ -0,0 +1,58 @@
+<?php get_header() ?>
+
+<section class="page-section">
+    <div class="container">
+
+        <div class="page-section__header">
+            <ul class="breadcrumbs__list">
+                <?php echo site_breadcrumbs(); ?>
+                
+            </ul>
+
+            <div class="page-section__title">
+                <h2><?php single_cat_title(); ?></h2>
+            </div>
+        </div>
+    </div>
+
+    <div class="container-nopaddings">
+        <div class="page-section__content">
+
+            <div class="container">
+ <?php
+
+            if (have_posts()) {
+            ?>
+                <ul class="post-block__list">
+                    <?php
+                    while (have_posts()) {
+                        the_post();
+                    ?>
+                        <?php get_template_part('templates/post-item'); ?>
+                    <?php
+                    }
+                    ?>
+                </ul>
+            <?php
+            } else {
+            }
+
+            wp_reset_postdata();
+
+            ?>
+           <?php get_template_part('templates/loadmore') ?>
+            
+            </div>
+            
+
+           
+
+        </div>
+    </div>
+
+
+    </div>
+</section>
+
+
+<?php get_footer() ?>
