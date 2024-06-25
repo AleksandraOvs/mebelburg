6706400017ac60901e90e987ccc2af8ae14bf166
Aleksandraovs <aleksandra.ovsia@gmail.com>
2024-06-25 15:57:48 +0300

    eds

--- wp-content/themes/mebelburg/page-news.php
+++ wp-content/themes/mebelburg/page-news.php
@@ -33,26 +33,32 @@
             <div class="container">
  <?php
  
+ $args = array(
+	'posts_per_page' => 9,
+	'cat' => 'news'
+);
 
-            if (have_posts()) {
-            ?>
-                <ul class="post-block__list">
-                    <?php
-                    while (have_posts()) {
-                        the_post();
-                    ?>
-                        <?php get_template_part('templates/post-item'); ?>
-                    <?php
-                    }
-                    ?>
-                </ul>
-            <?php
-            } else {
-            }
+$query = new WP_Query( $args );
 
-            wp_reset_postdata();
+// Цикл
+if ( $query->have_posts() ) {
+	echo '<ul class="post-block__list">';
+	while ( $query->have_posts() ) {
+		$query->the_post();
+		?>
+        <?php get_template_part('templates/post-item'); ?>
+        <?php
+	}
+	echo '</ul>';
+}
+else {
+	// Постов не найдено
+}
 
-            ?>
+// Возвращаем оригинальные данные поста. Сбрасываем $post.
+wp_reset_postdata();
+
+?>
            <?php get_template_part('templates/loadmore') ?>
             
             </div>
