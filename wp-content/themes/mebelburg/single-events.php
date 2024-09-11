bc71b630a7dc9ba7109fb0bef86b3a74ca57c609
Aleksandraovs <aleksandra.ovsia@gmail.com>
2024-09-11 10:09:07 +0300

    add single-events

---
+++ wp-content/themes/mebelburg/single-events.php
@@ -0,0 +1,24 @@
+<?php get_header() ?>
+
+<section class="page-section _single-template" style="padding-top: 150px;">
+    <div class="container">
+        <div class="page-section__header">
+            <ul class="breadcrumbs__list">
+                <?php //echo site_breadcrumbs(); ?>
+            </ul>
+
+            <div class="page__title _single-template__title">
+                <h2><?php the_title() ?></h2>
+                <p class="_single-template__time"><?php the_time('j.m.Y'); ?></p>
+            </div>
+        </div>
+    </div>
+
+    <div class="container _single-template__content">
+        <?php the_content() ?>
+    </div>
+
+
+</section>
+
+<?php get_footer() ?>
