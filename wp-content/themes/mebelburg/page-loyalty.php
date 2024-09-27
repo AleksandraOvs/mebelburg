ded45e50dc1b4b2ed1e4e83070f78fdd4ee32808
Aleksandraovs <aleksandra.ovsia@gmail.com>
2024-09-27 12:32:14 +0300

    eds

--- wp-content/themes/mebelburg/page-loyalty.php
+++ wp-content/themes/mebelburg/page-loyalty.php
@@ -195,7 +195,7 @@ get_header() ?>
             <p class="form-block__description">
             Оставьте заявку на сайте, и&nbsp;наш менеджер свяжется с&nbsp;вами, чтобы уточнить детали и&nbsp;подтвердить участие
             </p>
-            <?php if ($loyalty_contactform = carbon_get_post_meta(get_the_ID(), 'crb_cf_loyalty')) {
+            <?php if ($loyalty_contactform = carbon_get_theme_option('crb_cf_loyalty')) {
                 echo do_shortcode(" $loyalty_contactform ");
             } ?>
             <?php //echo do_shortcode('[contact-form-7 id="8b26afb" title="Форма (реклама)"]')
