1e7936b8765eef97ae0a60597830c89b6fbb8b04
aleksandra <aleksandra.ovsia@gmail.com>
2024-10-09 17:57:20 +0300

    edit posts autors

--- wp-content/themes/mebelburg/archive-check.php
+++ wp-content/themes/mebelburg/archive-check.php
@@ -10,7 +10,6 @@
 
 <section class="page-section">
     <div class="container">
-
         <div class="page-section__header">
             <ul class="breadcrumbs__list">
                 <?php echo site_breadcrumbs(); ?>
@@ -20,56 +19,95 @@
                 <h2>Загруженные чеки</h2>
             </div>
 
-            <div class="page-section__description _events-desc">
+            <div class="page-section__description">
                 <p>
-                   
+                    На этой странице публикуются чеки, добавленные пользователями
                 </p>
             </div>
         </div>
     </div>
 
-    <div class="container-nopaddings">
-        <div class="page-section__content">
+    <div class="container">
+        <?php
+        $number = 10;
 
-            <div class="container">
-                <?php
+        $paged = (get_query_var('paged')) ? get_query_var('paged') : 1;
 
-                if (have_posts()) {
-                ?>
-                    <ul class="post-block__list">
+        $offset = ($paged - 1) * $number;
+
+        $users = get_users();
+
+        $total_users = count($users);
+
+        $query = get_users('&offset=' . $offset . '&number=' . $number);
+
+        $total_pages = intval($total_users / $number) + 1;
+
+        echo '<ul id="users">';
+        foreach ($query as $q) { ?>
+
+            <li class="user clearfix">
+                <!-- Краткая информация о пользователе -->
+                <ul class="user-data">
+                    <li class="user-login">
+                        <span>Логин:&nbsp;</span><?php echo get_the_author_meta('display_name', $q->ID); ?>
+                    </li>
+                    <li class="user-email">
+                        <span>E-mail:&nbsp;</span><?php echo get_the_author_meta('user_email', $q->ID); ?>
+                    </li>
+                    <li class="user-register">
+                        <span>Дата регистрации:&nbsp;</span><?php echo get_the_author_meta('user_registered', $q->ID); ?>
+                    </li>
+                    <?php $author_id = get_the_author_meta('id', $q->ID); ?>
+                </ul>
+
+                <ul class="user-posts">
+                    <?php
+                    $args = array(
+                        'author' => $author_id,
+                        'order'       => 'ASC',
+                        'post_type'   => 'check',
+                        'suppress_filters' => true,
+                    );
+                    $posts = get_posts($args);
+                    foreach ($posts as $post) {
+                        setup_postdata($post); ?>
+                      
+                        <li class="post-block__item">
+                        <?php if ($thumb_url_full = get_the_post_thumbnail_url( $post->post_id, 'full' )){
+                             $thumb_url_medium = get_the_post_thumbnail_url( $post->post_id, 'medium' ) ?>
+                        <a class="check-image-link" data-fancybox="gallery" href="<?php echo $thumb_url_full ?>">
+                            <img src="<?php echo $thumb_url_medium ?>" alt="">
+                        </a>
                         <?php
-                        while (have_posts()) {
-                            the_post();
-                        ?>
-                            <?php get_template_part('templates/post-check'); ?>
-                        <?php
+                        }else {
+                            echo '<img src="'. get_stylesheet_directory_uri().'/images/svg/placeholder.svg" alt="">';
                         }
                         ?>
-                    </ul>
-                <?php
-                } else {
-                ?>
 
-                    <h2 class="enter__title_events">
-                        Скоро <span>тут</span> появятся<br>мероприятия
-                    </h2>
+                        <div class="post-check-content">
+                            <h4><?php the_title() ?></h4>
+                            <?php the_date( 'Y-m-d', '<p>', '</p>' ); ?>
+                        </div>
 
-                <?php
-                }
+                        
+                       
+                        </li>
+                    <?php
+                    }
+                    wp_reset_postdata();
+                    ?>
+                </ul>
 
-                wp_reset_postdata();
-
-                ?>
-                <?php //get_template_part('templates/loadmore') 
-                ?>
-
-            </div>
-
-
-
-
-        </div>
+            </li>
+        <?php }
+        echo '</ul>';
+        ?>
     </div>
+
+
+
+
 
 
     </div>
