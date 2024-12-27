688457ee67a6aadcbcf82faa4348d0966ee83b71
aleksandra <aleksandra.ovsia@gmail.com>
2024-12-27 12:13:23 +0300

    eds

--- wp-content/themes/mebelburg/templates/award-item.php
+++ wp-content/themes/mebelburg/templates/award-item.php
@@ -1,29 +1,28 @@
 <li class="sale-block__item">
-    <?php
-    if (has_post_thumbnail()) {
-
+<?php
+if( has_post_thumbnail() ) {
+       
         the_post_thumbnail();
-    } else {
-    ?>
-        <img src=" <?php echo get_stylesheet_directory_uri() ?>/images/svg/placeholder.svg" alt="">
-    <?php
+       
+    }else {
+        ?>
+      
+             <img src=" <?php echo get_stylesheet_directory_uri()?>/images/svg/placeholder.svg" alt="">
+      
+       
+        <?php
     }
-    ?>
+?>
     <a href="<?php the_permalink() ?>" class="sale-block__item__content">
         <div class="sale-block__item__content__head">
 
-            <?php
-            //if ($sale_date = carbon_get_post_meta(get_the_ID(), 'crb_sale_date') ){
-            ?>
-            <!-- <div class="sale-block__item__content__head__date">
-                        <?php //echo $sale_date 
-                        ?>
-                    </div> -->
-            <?php
-            //}
-            ?>
-            &nbsp;
-
+               
+                    <div class="sale-block__item__content__head__date">
+                        <?php the_time('j.m.Y'); ?>
+                    </div>
+                    
+                &nbsp;
+            
             <div class="item__link">
                 <svg width="14" height="14" viewBox="0 0 14 14" fill="none" xmlns="http://www.w3.org/2000/svg">
                     <path d="M13 8V1H6" stroke="#0D0D0D" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
@@ -32,10 +31,9 @@
 
             </div>
         </div>
-
-
-        <div class="sale-block__item__content__bottom"><?php the_title() ?>
-    </div>
-
-    </a>
+        <div class="sale-block__item__content__bottom"><?php the_title() ?></div>
+</a>
+      
+        
+       
 </li>
