7ad70c188525e2146ca4ec6c6896f66f143663a9
aleksandra <aleksandra.ovsia@gmail.com>
2024-12-27 23:30:24 +0300

    eds

--- wp-content/themes/mebelburg/templates/award-item.php
+++ wp-content/themes/mebelburg/templates/award-item.php
@@ -5,10 +5,7 @@
         the_post_thumbnail();
     } else {
     ?>
-
         <img src=" <?php echo get_stylesheet_directory_uri() ?>/images/svg/placeholder.svg" alt="">
-
-
     <?php
     }
     ?>
@@ -24,10 +21,7 @@
             <?php
             }
             ?>
-
-
             &nbsp;
-
             <div class="item__link">
                 <svg width="14" height="14" viewBox="0 0 14 14" fill="none" xmlns="http://www.w3.org/2000/svg">
                     <path d="M13 8V1H6" stroke="#0D0D0D" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
@@ -36,9 +30,6 @@
 
             </div>
         </div>
-        <div class="sale-block__item__content__bottom"><?php the_excerpt() ?></div>
+        <!-- <div class="sale-block__item__content__bottom"><?php //the_excerpt() ?></div> -->
     </a>
-
-
-
 </li>
